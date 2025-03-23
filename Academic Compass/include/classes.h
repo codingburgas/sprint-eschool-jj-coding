eac7ef63e8bfe6b229e2ce859b5302d793298231
KIKoev23 <KIKoev23@codingburgas.bg>
2025-03-23 21:26:19 +0200

    classes

--- Academic Compass/include/classes.h
+++ Academic Compass/include/classes.h
@@ -1,6 +1,19 @@
 #ifndef CLASSES_H
+#include <string>
 #define CLASSES_H
+using namespace std;
 
 void classesMenu();
 
 #endif
+
+#ifndef SUBJECTS_H
+#define SUBJECTS_H
+
+
+
+void showMenu();
+void biologyExercise();
+void chemistryExercise();
+void physicsExercise();
+void geographyExercise();
