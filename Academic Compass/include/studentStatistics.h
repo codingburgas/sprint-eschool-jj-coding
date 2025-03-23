699c9674216925f6d4d6065fedeff8a898e65ec7
Velizar Vulkov <VTVulkov23@codingburgas.bg>
2025-03-23 19:44:52 +0200

    Students statistics

--- Academic Compass/include/studentStatistics.h
+++ Academic Compass/include/studentStatistics.h
@@ -1,6 +1,18 @@
 #ifndef STUDENTSTATISTICS_H
 #define STUDENTSTATISTICS_H
-
+#include <string>
+using namespace std;
 void studentStatistics();
+
+
+
+struct Student {
+    string name;
+    double grade;
+};
+
+void inputStudents(Student students[], int size);
+void sortStudents(Student students[], int size);
+void printStudents(const Student students[], int size);
 
 #endif
--- Academic Compass/src/studentStatistics.cpp
+++ Academic Compass/src/studentStatistics.cpp
@@ -5,4 +5,32 @@ using namespace std;
 void studentStatistics() {
     system("clear");
     cout << "Menu is opening" << endl;
+
+
+    void inputStudents(Student students[], int size) {
+        for (int i = 0; i < size; ++i) {
+            cout << "Въведете име на ученик №" << i + 1 << ": ";
+            cin.ignore();
+            getline(cin, students[i].name);
+            cout << "Въведете оценка на " << students[i].name << ": ";
+            cin >> students[i].grade;
+        }
+    }
+
+    void sortStudents(Student students[], int size) {
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                if (students[i].grade < students[j].grade) {
+                    swap(students[i], students[j]);
+                }
+            }
+        }
+    }
+
+    void printStudents(const Student students[], int size) {
+        cout << "\nСортиран списък на учениците (по оценки):\n";
+        for (int i = 0; i < size; ++i) {
+            cout << i + 1 << ". " << students[i].name << " - " << students[i].grade << endl;
+        }
+    }
 }
