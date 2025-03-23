f91ba66d82a75e0fed89bf6391225cc418c62198
Velizar Vulkov <VTVulkov23@codingburgas.bg>
2025-03-23 19:30:45 +0200

    Average grade

--- Academic Compass/src/calculateAverageGrade.cpp
+++ Academic Compass/src/calculateAverageGrade.cpp
@@ -5,4 +5,15 @@ using namespace std;
 void calculateAverageGrade() {
     system("clear");
     cout << "Menu is opening" << endl;
+
+
+    int main() {
+        double numbers[] = { 5.0, 10.0, 15.0, 20.0 };
+        int size = sizeof(numbers) / sizeof(numbers[0]);
+
+        double avg = calculateAverage(numbers, size);
+        cout << "Average grade: " << avg << endl;
+
+        
+    }
 }
